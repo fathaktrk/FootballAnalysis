b65fd1014070d218e10467b093330cfccde2777b
Mr.Y <75855527+YasirAltun@users.noreply.github.com>
2022-12-07 11:43:38 +0300

    Update HAPSeasonDal.cs

--- HtmlAgiltyPack/Concrete/HAPSeasonDal.cs
+++ HtmlAgiltyPack/Concrete/HAPSeasonDal.cs
@@ -20,15 +20,17 @@ namespace HtmlAgiltyPack.Concrete
         public HtmlWeb HtmlWeb { get; set; }
         public Season Season { get; set; }
 
-        public Season GetSeasonInfos(int year)
+        public Season GetSeasonInfos(int year)//sezon bilgilerinin çekildiği metod
         {
             //https://www.transfermarkt.com.tr/super-lig/startseite/wettbewerb/TR1/plus/?saison_id=2021
             string urlAdress = "https://www.transfermarkt.com.tr/super-lig/startseite/wettbewerb/TR1/plus/?saison_id=" + year;
             var doc = HtmlWeb.Load(urlAdress);
-            Season.Champion = doc.DocumentNode.SelectSingleNode(xpath: "//*[@id=\"yw3\"]/table/tbody/tr[1]/td[3]/a").InnerText;
+            Season.Champion = doc.DocumentNode.SelectSingleNode(xpath: "//*[@id=\"yw3\"]/table/tbody/tr[1]/td[3]/a").InnerText;//td3 de şampiyon adı yazıyor
             Season.SeasonYear = year;
             Season.GoalKing = doc.DocumentNode.SelectSingleNode(xpath: "//table[@class='inline-table']").InnerText.Replace("Santrafor", "").Replace("On Numara", "").Replace("Sağ Kanat", "").Replace("Sol Kanat", "").Replace("Merkez orta saha", "");
+            //inner textte verilen oyuncunun mevkisi verileri çekerken sorun çıkarttığı için hepsinin "" (null) olarak ayarladım
             Season.GoalKingScore = Convert.ToInt16(doc.DocumentNode.SelectSingleNode(xpath: "//*[@id=\"yw2\"]/table/tbody/tr[1]/td[5]").InnerText);
+            //td5 deattığı gol sayısı yazıyor
             return Season;
         }
 
